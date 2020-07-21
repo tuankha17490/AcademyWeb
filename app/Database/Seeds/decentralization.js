9a8a38b4f53324cad99f9f69e665badc8b12593c
tuankha17490 <tuankha1749@gmail.com>
2020-07-21 12:48:09 +0700

    seed permission

--- app/Database/Seeds/decentralization.js
+++ app/Database/Seeds/decentralization.js
@@ -58,7 +58,7 @@ const permissions = []
 for (let i = 0; i < modules.length; i++) {
   for (let y = 0; y < methods.length; y++) {
     permissions.push({
-      ID:count,
+      ID: count,
       Module_Id: modules[i].ID,
       Method_Id: methods[y].ID
     })
@@ -67,7 +67,7 @@ for (let i = 0; i < modules.length; i++) {
 }
 
 const admin_role_permission = []
-for(let i = 1; i < count; i++) {
+for (let i = 1; i < count; i++) {
   admin_role_permission.push({
     Role_Id: 1,
     Permission_Id: i
@@ -76,53 +76,147 @@ for(let i = 1; i < count; i++) {
 
 const student_role_permission = []
 
-modules.forEach(module => {
-  methods.forEach(method => {
-    if(module.Name == 'Users') {
-      if(method.Name == 'Read' || method.Name == 'UpdateMyUser') {
-        permissions.forEach(permission => {
-          if(permission.Method_Id == method.ID && permission.Module_Id == module.ID) {
-            student_role_permission.push({
-              Role_Id: 4,
-              Permission_Id: permission.ID
+function StudentRole(moduleName, methodNames = []) {
+  modules.forEach(module => {
+    if (moduleName == module.Name) {
+      for (let i = 0; i < methodNames.length; i++) {
+        methods.forEach(method => {
+          if (method.Name == methodNames[i]) {
+            permissions.forEach(permission => {
+              if (permission.Method_Id == method.ID && permission.Module_Id == module.ID) {
+                student_role_permission.push({
+                  Role_Id: 4,
+                  Permission_Id: permission.ID
+                })
+              }
             })
           }
-        })
+        });
       }
     }
-    // if(module.Name == 'Class') {
-    //   if(method.Name == 'Read' || method.Name == 'UpdateMyUser') {
-    //     permissions.forEach(permission => {
-    //       if(permission.Method_Id == method.ID && permission.Module_Id == module.ID) {
-    //         student_role_permission.push({
-    //           Role_Id: 4,
-    //           Permission_Id: permission.ID
-    //         })
-    //       }
-    //     })
-    //   }
-    // }
   })
-  
-});
+}
+
+const teacher_role_permission = []
+
+function TeacherRole(moduleName, methodNames = []) {
+  modules.forEach(module => {
+    if (moduleName == module.Name) {
+      for (let i = 0; i < methodNames.length; i++) {
+        methods.forEach(method => {
+          if (method.Name == methodNames[i]) {
+            permissions.forEach(permission => {
+              if (permission.Method_Id == method.ID && permission.Module_Id == module.ID) {
+                teacher_role_permission.push({
+                  Role_Id: 3,
+                  Permission_Id: permission.ID
+                })
+              }
+            })
+          }
+        });
+      }
+    }
+  })
+}
+
+const moderator_role_permission = []
+
+function ModeratorRole(moduleName, methodNames = []) {
+  modules.forEach(module => {
+    if (moduleName == module.Name) {
+      for (let i = 0; i < methodNames.length; i++) {
+        methods.forEach(method => {
+          if (method.Name == methodNames[i]) {
+            permissions.forEach(permission => {
+              if (permission.Method_Id == method.ID && permission.Module_Id == module.ID) {
+                moderator_role_permission.push({
+                  Role_Id: 2,
+                  Permission_Id: permission.ID
+                })
+              }
+            })
+          }
+        });
+      }
+    }
+  })
+
+}
+
+
+
+
+
+
+
+
+// modules.forEach(module => {
+//   methods.forEach(method => {
+//     if(module.Name == 'Users') {
+//       if(method.Name == 'Read' || method.Name == 'UpdateMyUser') {
+//         permissions.forEach(permission => {
+//           if(permission.Method_Id == method.ID && permission.Module_Id == module.ID) {
+//             student_role_permission.push({
+//               Role_Id: 4,
+//               Permission_Id: permission.ID
+//             })
+//           }
+//         })
+//       }
+//     }
+
+//   })
+
+// });
+
 
 exports.seed = async function (knex) {
+  await StudentRole('Users', ['UpdateMyUser'])
+  await StudentRole('Class', ['Read'])
+  await StudentRole('Subject', ['Read'])
+  await StudentRole('Post', ['Read'])
+
+  await TeacherRole('Users', ['UpdateMyUser'])
+  await TeacherRole('Class', ['Read'])
+  await TeacherRole('Subject', ['Read'])
+  await TeacherRole('Post', ['Read', 'Create', 'Update', 'Delete'])
+
+  await ModeratorRole('Users', ['Create', 'Read'])
+  await ModeratorRole('Class', ['Read'])
+  await ModeratorRole('Subject', ['Read', 'Create'])
+  await ModeratorRole('Post', ['Read'])
+
   // Deletes ALL existing entries
   await knex('Role_Permission').del();
   await knex('Permissions').del();
   await knex('Methods').del();
   await knex('Modules').del();
   await knex('Roles').del()
-  await knex('Roles').insert([
-    {ID: 1, Name: 'Admin'},
-    {ID: 2, Name: 'Moderator'},
-    {ID: 3, Name: 'Teacher'},
-    {ID: 4, Name: 'Student'}
+  await knex('Roles').insert([{
+      ID: 1,
+      Name: 'Admin'
+    },
+    {
+      ID: 2,
+      Name: 'Moderator'
+    },
+    {
+      ID: 3,
+      Name: 'Teacher'
+    },
+    {
+      ID: 4,
+      Name: 'Student'
+    }
   ]);
   await knex('Methods').insert(methods);
   await knex('Modules').insert(modules);
   await knex('Permissions').insert(permissions);
   await knex('Role_Permission').insert(admin_role_permission);
+  await knex('Role_Permission').insert(moderator_role_permission);
+  await knex('Role_Permission').insert(teacher_role_permission);
   await knex('Role_Permission').insert(student_role_permission);
+
 
 };
--- app/Database/Seeds/users.js
+++ app/Database/Seeds/users.js
@@ -8,7 +8,7 @@ const createUsers = () => ({
   Avatar: faker.internet.avatar(),
   Slug: faker.lorem.slug(),
   Gender: faker.random.boolean(),
-  Role_Id: 4
+  Role_Id: 4,
 })
 
 const createAdmin = {
@@ -21,9 +21,39 @@ const createAdmin = {
   Role_Id: 1
 }
 
+const createTeacher = {
+  Email: 'teacher@gmail.com',
+  Name: 'teacher',
+  Password: bcript.hashSync('teacher123',10),
+  Avatar: faker.internet.avatar(),
+  Slug: 'teacher',
+  Gender: true,
+  Role_Id: 3
+}
+const createModerator = {
+  Email: 'moderator@gmail.com',
+  Name: 'moderator',
+  Password: bcript.hashSync('moderator123',10),
+  Avatar: faker.internet.avatar(),
+  Slug: 'moderator',
+  Gender: true,
+  Role_Id: 2
+}
+const createStudent = {
+  Email: 'student@gmail.com',
+  Name: 'student',
+  Password: bcript.hashSync('student123',10),
+  Avatar: faker.internet.avatar(),
+  Slug: 'student',
+  Gender: true,
+  Role_Id: 4
+}
 exports.seed = async function (knex) {
   const fakeUsers = [];
   fakeUsers.push(createAdmin)
+  fakeUsers.push(createModerator)
+  fakeUsers.push(createTeacher)
+  fakeUsers.push(createStudent)
   const fakeUserAmount = 30
   for (let i = 0; i < fakeUserAmount; i++) {
     fakeUsers.push(createUsers())
